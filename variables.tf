493c6f71d59472272c496a8e2e65c43f4aa00d42
BF <bfrunza@anvibo.com>
2019-11-18 13:25:21 +0000

    first release

--- variables.tf
+++ variables.tf
@@ -1,14 +1,14 @@
 variable "cluster_name" {
-  
+
 }
 variable "cluster_location" {
-  
+
 }
 variable "network" {
-  
+
 }
 variable "subnetwork" {
-  
+
 }
 variable "resource_labels" {
   type = map
